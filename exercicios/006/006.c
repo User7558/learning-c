bd76b919f30b33d5f59fe42038337fd5dd8b3813
User7558 <82788197+User7558@users.noreply.github.com>
2024-05-11 16:53:44 -0300

    Update 006.c

--- exercicios/006/006.c
+++ exercicios/006/006.c
@@ -15,10 +15,9 @@ int main (void){
 
     fscanf(arq, "%d, %f", &cv, &vm);
     c1 = cv * 250.00;
-    c2 = vm * 0.3;
+    c2 = vm * 0.03;
     salario = c1 + c2 + 2000.00;
 
-    printf("%d, %f\n", cv, vm);
     printf("O salario do mes do vendedor sera de R$%.2f\n", salario);
 
     fclose(arq);
